b02e13455691e3d225892a33bb2a6a6abd94c394
Ishan Aditya <ishan.a@turtlemint.com>
2022-01-22 21:16:31 +0530

    add subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,4 +7,8 @@ module "vpc"{
 	version = "3.11.3"
 	name = var.vpc_name
 	cidr = "10.34.0.0/16"
+
+	azs             = var.az
+  	#private_subnets = var.
+    #public_subnets  = var.public_subnet
 }
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,3 +2,13 @@ variable "vpc_name"{
 	type = string
 	default = "terraform_demo_vpc"
 }
+
+variables "az"{
+	type = string
+	default = "ap-south-1a"
+}
+
+variable "public_subnet"{
+	type = string
+	default = "10.34.1.0/24"
+}
